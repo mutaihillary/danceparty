9b3b05624b7c6303168f7d2940013e2e7f7809f6
mutaihillary <mutaihillary@yahoo.com>
2016-07-15 14:53:57 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+  window.dancers = [];
+
+  $(".addDancerButton").on("click", function(event){
+    /* This function sets up the click handlers for the create-dancer
+     * buttons on dancefloor.html. You should only need to make one small change to it.
+     * As long as the "data-dancer-maker-function-name" attribute of a
+     * class="addDancerButton" DOM node matches one of the names of the
+     * maker functions available in the global scope, clicking that node
+     * will call the function to make the dancer.
+     */
+
+    /* dancerMakerFunctionName is a string which must match
+     * one of the dancer maker functions available in global scope.
+     * A new object of the given type will be created and added
+     * to the stage.
+     */
+    var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
+
+    // get the maker function for the kind of dancer we're supposed to make
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+
+    // make a dancer with a random position
+
+    var dancer = dancerMakerFunction(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
+    );
+    $('body').append(dancer.$node);
+  });
+  
+  $('.addDancerButton').mouseenter(function(e) {
+    alert('yo');});
+});
+
+var makeBlinkyDancer = function(top, left, timeBetweenSteps){
+  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  var oldStep = blinkyDancer.step;
+
+  blinkyDancer.step = function(){
+    // call the old version of step at the beginning of any call to this new version of step
+    oldStep();
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    blinkyDancer.$node.toggle();
+  };
+
+  return blinkyDancer;
+};
+
+var oldStep = blinkyDancer.step.call(this, arguments);
+
+
+// Creates and returns a new dancer object that can step
+var makeDancer = function(top, left, timeBetweenSteps){
+
+  var dancer = {};
+
+  // use jQuery to create an HTML <span> tag
+  dancer.$node = $('<span class="dancer"></span>');
+
+
+  dancer.step = function(){
+    // the basic dancer doesn't do anything interesting at all on each step,
+    // it just schedules the next step
+    setTimeout(dancer.step, timeBetweenSteps);
+  };
+  dancer.step();
+
+  dancer.setPosition = function(top, left){
+    // Use css top and left properties to position our <span> tag
+    // where it belongs on the page. See http://api.jquery.com/css/
+    //
+    var styleSettings = {
+      top: top,
+      left: left
+    };
+    dancer.$node.css(styleSettings);
+  };
+
+  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
+  // this one sets the position to some random default point within the body
+  dancer.setPosition(top, left);
+
+  return dancer;
+};
